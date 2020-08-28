522419b6d4aa5eb3255d35437511b743e857f913
Pierre LAURENT <PierreLrt72@gmail.com>
2020-08-28 12:03:08 +0200

    Display Book

---
+++ src/com/guill/servlets/DisplayBook.java
@@ -0,0 +1,60 @@
+package com.guill.servlets;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.guill.beans.Book;
+import com.school.bdd.ConnexionBDD;
+
+public class DisplayBook extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+		      throws ServletException, IOException {	
+		try {
+			/* Chargement du driver JDBC pour MySQL */
+			try {
+			    Class.forName( "com.mysql.jdbc.Driver" );
+			} catch ( ClassNotFoundException e ) {
+			    /* Gérer les éventuelles erreurs ici. */
+			}
+			ConnexionBDD conn = new ConnexionBDD("C:\\Users\\tarik\\git\\SchoolProject\\src\\SQL\\db.properties");
+			conn.seConnecter();
+			
+			ResultSet rs = conn.getBook();
+
+			List<Book> listBooks = new ArrayList<Book>();
+			
+			
+			while(rs.next()) {
+				Book b = new Book();
+				b.setRef(Integer.parseInt((rs.getString("ref"))));
+				b.setTitle(rs.getString("title"));
+				b.setAuthor(rs.getString("author"));
+				listBooks.add(c);
+			}
+		
+			
+			req.setAttribute("books", listBooks);	
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/Home.jsp" ).forward( req, res );	
+
+	}
+}
