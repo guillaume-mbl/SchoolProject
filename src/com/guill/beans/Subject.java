6658b4acd8652614437aa26b20929af10f6b33d6
EtoileNocturne <tarik_yildiz@hotmail.fr>
2020-08-28 09:39:51 +0200

    Student

---
+++ src/com/guill/beans/Subject.java
@@ -0,0 +1,16 @@
+package com.guill.beans;
+
+public class Subject {
+
+	private String nameSubject;
+
+	public String getNameSubject() {
+		return nameSubject;
+	}
+
+	public void setNameSubject(String nameSubject) {
+		this.nameSubject = nameSubject;
+	}
+	
+	
+}
